0972bf50480dcc6c4b27221e5b7be12897307e57
L. C. Sundar Raj <37692946+Lcsundarraj@users.noreply.github.com>
2020-10-08 08:24:38 +0530

    Create if_else.py

---
+++ if_else.py
@@ -0,0 +1,29 @@
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+
+
+a = 393
+b = 290
+if b > a:
+  print("b is greater than a")
+else:
+  print("a is greater than b")
+
+
+a = 290
+b = 290
+if b > a:
+  print("b is greater than a")
+elif a==b:
+  print("both are same")
+else:
+  print("a is greater than b")
+
+
+a = 333
+b = 33
+if b > a:print("b is greater than a")
+print("A") if a > b else print("=") if a == b else print("B")

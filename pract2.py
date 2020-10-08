7386b0b4c1f65c244d2295541c7b4354684bf5b6
L. C. Sundar Raj <37692946+Lcsundarraj@users.noreply.github.com>
2020-10-08 06:49:20 +0530

    string n more

--- pract2.py
+++ pract2.py
@@ -15,3 +15,24 @@ for mine in lappy:
 
 a = "Hello, World!"
 print(a.lower())
+
+#Tupple Examples
+#it gives cherry to kiwi (2-5  excluding 5)
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
+
+
+#change tuple to list (BUT TUPLE IS IMMUTABLE - u can not change values from a tuple)
+#You can convert the tuple into a list, change the list, and convert the list back into a tuple.
+
+#tuple
+lappy = ("hp", "dell", "Asus")
+print(lappy)
+#conert to list
+myLappyList = list(lappy)
+#modify the value
+myLappyList[1] = "Lenovo"
+#convert back to tuple
+lappy = tuple(myLappyList)
+
+print(lappy)

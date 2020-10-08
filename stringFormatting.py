987efb0856eaa0628b6bdb68851ffd0964f505c2
L. C. Sundar Raj <37692946+Lcsundarraj@users.noreply.github.com>
2020-10-08 08:00:49 +0530

    Update stringFormatting.py

--- stringFormatting.py
+++ stringFormatting.py
@@ -1,7 +1,7 @@
-person = 123
+person = "Raj"
 price = 100
 #'cannot switch from automatic field numbering to manual field specification' error occurs while adding default value in priceS placeholder
-Bill = "{1}\'s billing price is {3:.2f} dollars"
+Bill = "{}\'s billing price is {:.2f} dollars"
 print(Bill.format(person,price))
 
 
@@ -14,3 +14,12 @@ itemno = 567
 price = 49
 myorder = "I want {0} pieces of item number {1} for {2:.2f} dollars."
 print(myorder.format(quantity, itemno, price))
+
+
+
+
+print("=====================================")
+age = 31
+name = "Raj"
+details = "My name is {1}. I am {0} years old."
+print(details.format(age, name))
